0ceb8a746ad6c805614c1b5dd6ecbbcaa2a675af
Chunhui <hanchunhui@gmail.com>
2016-11-15 15:42:00 -0800

    new

---
+++ tutorial.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+import numpy as np
+
+#create 100 phony x, y data points in NumPy y = x * 0.1 + 0.3
+# x_data = np.random.rand(10).astype(np.float32)
+x_data = np.array([.1, .2, .3, .4, .5, .6, .7, .8, .9, .95]).astype(np.float32)
+y_data = x_data * 0.1 + 0.3
+print x_data
+W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+b = tf.Variable(tf.zeros([1]))
+y = W * x_data + b
+
+loss = tf.reduce_mean(tf.square(y - y_data))
+optimizer = tf.train.GradientDescentOptimizer(0.5)
+train = optimizer.minimize(loss)
+
+init = tf.initialize_all_variables()
+
+sess = tf.Session()
+sess.run(init)
+
+for step in range(201):
+    sess.run(train)
+    if step % 20 == 0:
+        print(step, sess.run(W), sess.run(b))
+
