b351b9ec30601e3bb34a70aa522102d0a6e1cc3c
Chunhui <hanchunhui@gmail.com>
2016-11-15 17:02:43 -0800

    update

--- tutorial.py
+++ tutorial.py
@@ -2,8 +2,8 @@ import tensorflow as tf
 import numpy as np
 
 #create 100 phony x, y data points in NumPy y = x * 0.1 + 0.3
-x_data = np.random.rand(10).astype(np.float32) * 100
-#x_data = np.array(list(range(100))).astype(np.float32)
+#x_data = np.random.rand(100).astype(np.float32) * 1
+x_data = np.array(list(range(100))).astype(np.float32)*.01
 y_data = x_data * 0.1 + 0.3
 print x_data
 W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
@@ -11,7 +11,9 @@ b = tf.Variable(tf.zeros([1]))
 y = W * x_data + b
 
 loss = tf.reduce_mean(tf.square(y - y_data))
-optimizer = tf.train.GradientDescentOptimizer(0.5)
+# The parameter for this optimizer function is crucial for the success of optimization.
+# Experiments show that the optimal value depends on the magnitude of x values.
+optimizer = tf.train.GradientDescentOptimizer(.5)
 train = optimizer.minimize(loss)
 
 init = tf.initialize_all_variables()
@@ -19,7 +21,7 @@ init = tf.initialize_all_variables()
 sess = tf.Session()
 sess.run(init)
 
-for step in range(201):
+for step in range(601):
     sess.run(train)
     if step % 20 == 0:
         print(step, sess.run(W), sess.run(b))
